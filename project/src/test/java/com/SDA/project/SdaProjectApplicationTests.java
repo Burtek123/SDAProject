0f7d2b1f9e59cb9323fe94ecc4dc253b1eb79b06
Burtek <burtek24@gmail.com>
2020-07-21 11:34:04 +0200

    Backend project created

---
+++ project/src/test/java/com/SDA/project/SdaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SDA.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

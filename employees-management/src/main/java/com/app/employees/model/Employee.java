e8c87d161430a46be7a4656f5c264d7957f99ba1
szczepan <szczepan_socha@yahoo.com>
2020-07-28 13:33:15 +0200

    update Employee class

--- employees-management/src/main/java/com/app/employees/model/Employee.java
+++ employees-management/src/main/java/com/app/employees/model/Employee.java
@@ -4,6 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
 
 @Document(value = "pracownicy")
 public class Employee {
@@ -14,6 +15,84 @@ public class Employee {
     private String firstName;
     @NotEmpty(message = "Last name is mandatory")
     private String lastName;
+    private String address;
+    @NotEmpty(message = "PESEL is required")
+    private String PESEL;
+    private int telephoneNumber;
+    private String position;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPESEL() {
+        return PESEL;
+    }
+
+    public void setPESEL(String PESEL) {
+        this.PESEL = PESEL;
+    }
+
+    public int getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(int telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return telephoneNumber == employee.telephoneNumber &&
+                Objects.equals(id, employee.id) &&
+                Objects.equals(firstName, employee.firstName) &&
+                Objects.equals(lastName, employee.lastName) &&
+                Objects.equals(address, employee.address) &&
+                Objects.equals(PESEL, employee.PESEL) &&
+                Objects.equals(position, employee.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, PESEL, telephoneNumber, position);
+    }
 }

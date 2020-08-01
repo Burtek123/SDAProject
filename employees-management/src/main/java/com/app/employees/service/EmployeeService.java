340d243b93946024f8d0d2bc8245e0f2bb0dd737
karobarca <karobarca@gmail.com>
2020-08-01 23:06:43 +0200

    add get method and app properties

--- employees-management/src/main/java/com/app/employees/service/EmployeeService.java
+++ employees-management/src/main/java/com/app/employees/service/EmployeeService.java
@@ -1,12 +1,19 @@
 package com.app.employees.service;
 
+import com.app.employees.model.Employee;
 import com.app.employees.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
 }
